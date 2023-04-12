d143a15a1d78f3d847ba745cc5ef4af9dc8e7885
leemathew from Mac Air <leemathew1998@gmail.com>
2023-04-12 23:42:54 +0800

    tijiao

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,9 @@
-import Vue from "vue";
-import axios from "axios";
-import store from "@/store";
-import { VueAxios } from "./axios";
-import router from "@/router/index";
-import { ACCESS_TOKEN, TENANT_ID } from "@/store/mutation-types";
+import Vue from 'vue'
+import axios from 'axios'
+import store from '@/store'
+import { VueAxios } from './axios'
+import router from '@/router/index'
+import { ACCESS_TOKEN, TENANT_ID } from '@/store/mutation-types'
 /**
  * 【指定 axios的 baseURL】
  * 如果手工指定 baseURL: '/jeecg-boot'
@@ -11,203 +11,203 @@ import { ACCESS_TOKEN, TENANT_ID } from "@/store/mutation-types";
  * @type {*|string}
  */
 // let apiBaseUrl = window._CONFIG['domianURL'] || '/jeecg-boot'
-let apiBaseUrl = "/api/";
+let apiBaseUrl = '/api/'
 // console.log("apiBaseUrl= ",apiBaseUrl)
 // 创建 axios 实例
 const service = axios.create({
   // baseURL: '/jeecg-boot',
   baseURL: apiBaseUrl, // api base_url
-  timeout: 7000, // 请求超时时间
-});
+  timeout: 90000 // 请求超时时间
+})
 
 const err = (error) => {
   if (error.response) {
-    let data = error.response.data;
-    const token = Vue.ls.get(ACCESS_TOKEN);
-    console.log("------异常响应------", token);
-    console.log("------异常响应------", error.response.status);
+    let data = error.response.data
+    const token = Vue.ls.get(ACCESS_TOKEN)
+    console.log('------异常响应------', token)
+    console.log('------异常响应------', error.response.status)
     switch (error.response.status) {
       case 403:
         Vue.prototype.$Jnotification.error({
-          message: "系统提示",
-          description: "拒绝访问",
-          duration: 4,
-        });
-        break;
+          message: '系统提示',
+          description: '拒绝访问',
+          duration: 4
+        })
+        break
       case 500:
-        console.log("------error.response------", error.response);
+        console.log('------error.response------', error.response)
         // update-begin- --- author:liusq ------ date:20200910 ---- for:处理Blob情况----
-        let type = error.response.request.responseType;
-        if (type === "blob") {
-          blobToJson(data);
-          break;
+        let type = error.response.request.responseType
+        if (type === 'blob') {
+          blobToJson(data)
+          break
         }
         // update-end- --- author:liusq ------ date:20200910 ---- for:处理Blob情况----
-        if (token && data.message.includes("Token失效")) {
+        if (token && data.message.includes('Token失效')) {
           // update-begin- --- author:scott ------ date:20190225 ---- for:Token失效采用弹框模式，不直接跳转----
           if (/wxwork|dingtalk/i.test(navigator.userAgent)) {
-            Vue.prototype.$Jmessage.loading("登录已过期，正在重新登陆", 0);
+            Vue.prototype.$Jmessage.loading('登录已过期，正在重新登陆', 0)
           } else {
             Vue.prototype.$Jmodal.error({
-              title: "登录已过期",
-              content: "很抱歉，登录已过期，请重新登录",
-              okText: "重新登录",
+              title: '登录已过期',
+              content: '很抱歉，登录已过期，请重新登录',
+              okText: '重新登录',
               mask: false,
               onOk: () => {
-                store.dispatch("Logout").then(() => {
-                  Vue.ls.remove(ACCESS_TOKEN);
+                store.dispatch('Logout').then(() => {
+                  Vue.ls.remove(ACCESS_TOKEN)
                   try {
-                    let path = window.document.location.pathname;
-                    console.log("location pathname -> " + path);
-                    if (path != "/" && path.indexOf("/user/login") == -1) {
-                      window.location.reload();
+                    let path = window.document.location.pathname
+                    console.log('location pathname -> ' + path)
+                    if (path != '/' && path.indexOf('/user/login') == -1) {
+                      window.location.reload()
                     }
                   } catch (e) {
-                    window.location.reload();
+                    window.location.reload()
                   }
-                });
-              },
-            });
+                })
+              }
+            })
           }
           // update-end- --- author:scott ------ date:20190225 ---- for:Token失效采用弹框模式，不直接跳转----
         }
-        break;
+        break
       case 404:
         Vue.prototype.$Jnotification.error({
-          message: "系统提示",
-          description: "很抱歉，资源未找到!",
-          duration: 4,
-        });
-        break;
+          message: '系统提示',
+          description: '很抱歉，资源未找到!',
+          duration: 4
+        })
+        break
       case 504:
         Vue.prototype.$Jnotification.error({
-          message: "系统提示",
-          description: "网络超时",
-        });
-        break;
+          message: '系统提示',
+          description: '网络超时'
+        })
+        break
       case 401:
         Vue.prototype.$Jnotification.error({
-          message: "系统提示",
-          description: "很抱歉，登录已过期，请重新登录",
-          duration: 4,
-        });
+          message: '系统提示',
+          description: '很抱歉，登录已过期，请重新登录',
+          duration: 4
+        })
         if (token) {
-          store.dispatch("Logout").then(() => {
+          store.dispatch('Logout').then(() => {
             setTimeout(() => {
-              window.location.reload();
-            }, 1500);
-          });
+              window.location.reload()
+            }, 1500)
+          })
         }
-        break;
+        break
       default:
         Vue.prototype.$Jnotification.error({
-          message: "系统提示",
+          message: '系统提示',
           description: data.message,
-          duration: 4,
-        });
-        break;
+          duration: 4
+        })
+        break
     }
   } else if (error.message) {
-    if (error.message.includes("timeout")) {
+    if (error.message.includes('timeout')) {
       Vue.prototype.$Jnotification.error({
-        message: "系统提示",
-        description: "网络超时",
-      });
+        message: '系统提示',
+        description: '网络超时'
+      })
     } else {
       Vue.prototype.$Jnotification.error({
-        message: "系统提示",
-        description: error.message,
-      });
+        message: '系统提示',
+        description: error.message
+      })
     }
   }
-  return Promise.reject(error);
-};
+  return Promise.reject(error)
+}
 
 // request interceptor
 service.interceptors.request.use(
   (config) => {
-    console.log("request config", config);
-    config.headers["Authorization"] = store.getters.token; // 让每个请求携带自定义 token 请根据实际情况自行修改
-    if (config.method == "get") {
-      if (config.url.indexOf("sys/dict/getDictItems") < 0) {
+    console.log('request config', config)
+    config.headers['Authorization'] = store.getters.token // 让每个请求携带自定义 token 请根据实际情况自行修改
+    if (config.method == 'get') {
+      if (config.url.indexOf('sys/dict/getDictItems') < 0) {
         config.params = {
-          ...config.params,
-        };
+          ...config.params
+        }
       }
     }
     // if (config.url.indexOf("/file/upload") !== -1) {
     //   config.headers["Content-Type"] = "multipart/form-data";
     // }
-    return config;
+    return config
   },
   (error) => {
-    console.log(error, "request error");
-    return Promise.reject(error);
+    console.log(error, 'request error')
+    return Promise.reject(error)
   }
-);
+)
 
 // response interceptor
 service.interceptors.response.use((response) => {
   // if (response.data.code == '20001' || response.data.code == '30000' ||
   // 	response.data.code == '40040' || response.data.code == '50000') {
   if (
-    response.data.code == "30000" ||
-    response.data.code == "40040" ||
-    response.data.code == "50000"
+    response.data.code == '30000' ||
+    response.data.code == '40040' ||
+    response.data.code == '50000'
   ) {
     // token 失效，重新登陆
-    console.log("token失效");
-    console.log("response", response);
-    store.commit("clearUserInfo", []);
-    console.log("与张升测试ISC，在此处修改跳转，如需要改变，直接切换即可！");
-    router.push("/overView");
+    console.log('token失效')
+    console.log('response', response)
+    store.commit('clearUserInfo', [])
+    console.log('与张升测试ISC，在此处修改跳转，如需要改变，直接切换即可！')
+    router.push('/overView')
     // router.push("/user/login");
   } else {
     // console.log('else')
-    return response.data;
+    return response.data
   }
-}, err);
+}, err)
 
 const installer = {
   vm: {},
   install(Vue, router = {}) {
-    Vue.use(VueAxios, router, service);
-  },
-};
+    Vue.use(VueAxios, router, service)
+  }
+}
 /**
  * Blob解析
  * @param data
  */
 function blobToJson(data) {
-  let fileReader = new FileReader();
-  let token = Vue.ls.get(ACCESS_TOKEN);
+  let fileReader = new FileReader()
+  let token = Vue.ls.get(ACCESS_TOKEN)
   fileReader.onload = function () {
     try {
-      let jsonData = JSON.parse(this.result); // 说明是普通对象数据，后台转换失败
-      console.log("jsonData", jsonData);
+      let jsonData = JSON.parse(this.result) // 说明是普通对象数据，后台转换失败
+      console.log('jsonData', jsonData)
       if (jsonData.status === 500) {
-        console.log("token----------》", token);
-        if (token && jsonData.message.includes("Token失效")) {
+        console.log('token----------》', token)
+        if (token && jsonData.message.includes('Token失效')) {
           Modal.error({
-            title: "登录已过期",
-            content: "很抱歉，登录已过期，请重新登录",
-            okText: "重新登录",
+            title: '登录已过期',
+            content: '很抱歉，登录已过期，请重新登录',
+            okText: '重新登录',
             mask: false,
             onOk: () => {
-              store.dispatch("Logout").then(() => {
-                Vue.ls.remove(ACCESS_TOKEN);
-                window.location.reload();
-              });
-            },
-          });
+              store.dispatch('Logout').then(() => {
+                Vue.ls.remove(ACCESS_TOKEN)
+                window.location.reload()
+              })
+            }
+          })
         }
       }
     } catch (err) {
       // 解析成对象失败，说明是正常的文件流
-      console.log("blob解析fileReader返回err", err);
+      console.log('blob解析fileReader返回err', err)
     }
-  };
-  fileReader.readAsText(data);
+  }
+  fileReader.readAsText(data)
 }
 
-export { installer as VueAxios, service as axios };
+export { installer as VueAxios, service as axios }
--- src/views/basic/tables/consumer.vue
+++ src/views/basic/tables/consumer.vue
@@ -27,102 +27,102 @@
   </div>
 </template>
 <script>
-import Tables from "@/components/tables/baseTables";
-import NewModel from "@/components/NewModel/consumerModel";
-import SearchForm from "@/components/searchform/consumerSearch";
-import { getAction, postAction } from "@/api/manage";
+import Tables from '@/components/tables/baseTables'
+import NewModel from '@/components/NewModel/consumerModel'
+import SearchForm from '@/components/searchform/consumerSearch'
+import { postAction } from '@/api/manage'
 const columns = [
   {
-    title: "用户编号",
-    dataIndex: "consNo",
-    align: "center",
+    title: '用户编号',
+    dataIndex: 'consNo',
+    align: 'center',
     ellipsis: true,
-    width: 120,
+    width: 120
   },
   {
-    title: "用户名称",
-    dataIndex: "consName",
+    title: '用户名称',
+    dataIndex: 'consName',
     ellipsis: true,
-    align: "center",
-    width: 150,
+    align: 'center',
+    width: 150
   },
   {
-    title: "供电单位编号",
-    dataIndex: "orgNo",
+    title: '供电单位编号',
+    dataIndex: 'orgNo',
     ellipsis: true,
-    align: "center",
-    width: 120,
+    align: 'center',
+    width: 120
   },
   {
-    title: "用户地址",
-    dataIndex: "elecAddr",
+    title: '用户地址',
+    dataIndex: 'elecAddr',
     ellipsis: true,
-    align: "center",
-    width: 170,
+    align: 'center',
+    width: 170
   },
   {
-    title: "用户状态",
-    dataIndex: "statusCode",
-    align: "center",
-    width: 100,
+    title: '用户状态',
+    dataIndex: 'statusCode',
+    align: 'center',
+    width: 100
   },
   {
-    title: "用户分类",
-    dataIndex: "consSortCode",
-    align: "center",
-    width: 135,
+    title: '用户分类',
+    dataIndex: 'consSortCode',
+    align: 'center',
+    width: 135
   },
   {
-    title: "行业分类",
-    dataIndex: "tradeCode",
+    title: '行业分类',
+    dataIndex: 'tradeCode',
     ellipsis: true,
-    align: "center",
-    width: 130,
+    align: 'center',
+    width: 130
   },
   {
-    title: "供电电压",
-    dataIndex: "voltCode",
+    title: '供电电压',
+    dataIndex: 'voltCode',
     ellipsis: true,
-    align: "center",
-    width: 80,
+    align: 'center',
+    width: 80
   },
   {
-    title: "合同容量(kVA)",
-    dataIndex: "contractCap",
-    align: "center",
-    width: 130,
+    title: '合同容量(kVA)',
+    dataIndex: 'contractCap',
+    align: 'center',
+    width: 130
   },
   {
-    title: "临时用电标志",
-    dataIndex: "tmpFlag",
-    align: "center",
-    width: 80,
+    title: '临时用电标志',
+    dataIndex: 'tmpFlag',
+    align: 'center',
+    width: 80
   },
   {
-    title: "抄表段编号",
-    dataIndex: "mrSectNo",
-    align: "center",
-    width: 90,
+    title: '抄表段编号',
+    dataIndex: 'mrSectNo',
+    align: 'center',
+    width: 90
   },
   {
-    title: "用户类别",
-    dataIndex: "elecTypeCode",
-    align: "center",
-    width: 130,
+    title: '用户类别',
+    dataIndex: 'elecTypeCode',
+    align: 'center',
+    width: 130
   },
   {
-    title: "运行容量(kVA)",
-    dataIndex: "runCap",
-    align: "center",
-    width: 130,
+    title: '运行容量(kVA)',
+    dataIndex: 'runCap',
+    align: 'center',
+    width: 130
   },
   {
-    title: "抄表员姓名",
-    dataIndex: "realName",
-    align: "center",
-    width: 80,
-  },
-];
+    title: '抄表员姓名',
+    dataIndex: 'realName',
+    align: 'center',
+    width: 80
+  }
+]
 export default {
   data() {
     return {
@@ -135,67 +135,67 @@ export default {
       // 弹窗数据
       ccontactData: [],
       cinformationData: [],
-      electricityPriceData: [],
-    };
+      electricityPriceData: []
+    }
   },
   components: {
     Tables,
     SearchForm,
-    NewModel,
+    NewModel
   },
   created() {
-    this.initList();
+    this.initList()
   },
   methods: {
     // 数据展示分装
     async initList(
       payload = {
-        consNo: "",
-        consName: "",
-        orgName: "",
-        elecTypeCode: "",
+        consNo: '',
+        consName: '',
+        orgName: '',
+        elecTypeCode: ''
       }
     ) {
-      this.loading = true;
-      this.data = [];
+      this.loading = true
+      this.data = []
       const res = await postAction(
         `coll/getConsListweb?consNo=${payload.consNo}&consName=${payload.consName}&orgName=${payload.orgName}&elecTypeCode=${payload.elecTypeCode}`
-      );
+      )
       if (
         res &&
         res.length > 0 &&
-        !(res.hasOwnProperty("code") && res.code == 20001)
+        !(res.hasOwnProperty('code') && res.code == 20001)
       ) {
-        console.log("基本信息", res);
-        this.data = [...res];
+        console.log('基本信息', res)
+        this.data = [...res]
       }
-      this.loading = false;
+      this.loading = false
     },
     // 搜索
     solveformData(e) {
       for (let key in e) {
         if (!e[key]) {
-          e[key] = "";
+          e[key] = ''
         }
       }
-      console.log("e", e);
-      this.initList(e);
+      console.log('e', e)
+      this.initList(e)
     },
     changeSelectedRowKeys(e) {
-      this.selectedRowKeys = e;
+      this.selectedRowKeys = e
     },
     // 处理点击进入详情的数据
     clickRows(e) {
-      this.ccontactData = [];
+      this.ccontactData = []
       postAction(`coll/getConsListMobile?consNo=${e.consNo}`).then((res) => {
-        console.log("电话：", res);
-        this.ccontactData = res;
-      });
-      this.cinformationData = e;
-      this.NewModalVisible = true;
-    },
-  },
-};
+        console.log('电话：', res)
+        this.ccontactData = res
+      })
+      this.cinformationData = e
+      this.NewModalVisible = true
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
